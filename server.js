d4efbb89454d57db05545b421bab79ae953e10ef
Anthonyhelka <helkaanthony1997@gmail.com>
2021-07-11 04:35:24 -0400

    Built

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('./dist/riftmaker-frontend'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/riftmaker-frontend/'}),
+  res.sendFile('index.html', { root: 'dist/riftmaker-frontend/' }),
 );
 
 app.listen(process.env.PORT || 8080);
