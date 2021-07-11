be3183563d84e49443b046b18721a625387b43e8
Anthonyhelka <helkaanthony1997@gmail.com>
2021-07-11 04:45:51 -0400

    Changes to heroku setup

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/riftmaker-frontend'));
+app.use(express.static(__dirname + '/dist/riftmaker-frontend'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: 'dist/riftmaker-frontend/' }),
-);
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/riftmaker-frontend/index.html'));
+});
 
 app.listen(process.env.PORT || 8080);
+
+console.log('Console Listening!');
