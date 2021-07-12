09d42e6dd48991d18a533ff096056b97d5dd9ac3
Anthonyhelka <helkaanthony1997@gmail.com>
2021-07-12 04:55:19 -0400

    Added validation check

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,6 +12,7 @@ export class HomeComponent {
   constructor(private router: Router) { }
 
   onSearch() {
+    if (this.searchTerm.trim() === '') { return; }
     this.router.navigate(['summoner', this.searchTerm]);
   }
 
